ec49fb72fb627b7445a3fc61666cdb356c7757b3
joaorihanfiap <rm99656@fiap.com.br>
2023-04-18 10:49:58 -0300

    B VNHBNHVJKM,,.LÇÇ,KJHGBVFCDSERFTGYHJKL,Ç.~] ~ÇLKJHGFCV BNJMK,LÇ.~;LPÇLKJIHUGFC

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,5 +1,6 @@
 
 const liElement = document.getElementsByTagName("li");
+const liElementArray = [...liElement]
 console.log(liElement)
 
 for (let index = 0; index < liElement.length; index++) {
@@ -10,4 +11,8 @@ let num1 = [1,2,3,4,5,];
 let num2 = [6,7,8,9,0];
 let num3 = [num1,num2];
 
-console.log(num3)
+num3.map((numero,key)=>{
+    console.log(`${key+1}Itens do array - ${numero}`);
+    numero.forEach
+    }
+)
