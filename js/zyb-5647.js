96efa186218258831cc5f6bd80e474fcf4024fe2
joaorihanfiap <rm99656@fiap.com.br>
2023-04-18 10:24:57 -0300

    Atualizei umas paradas paizão, tmj ai

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -6,8 +6,8 @@ for (let index = 0; index < liElement.length; index++) {
     console.log(liElement[index].textContent)   
 }
 
-let num1 = [1,2,3,4,5];
+let num1 = [1,2,3,4,5,];
 let num2 = [6,7,8,9,0];
-let num3 = num1 + num2
+let num3 = [num1,num2];
 
 console.log(num3)
