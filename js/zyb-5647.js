cde14efc3c785c2892086d6952705c5382930120
joaorihanfiap <rm99656@fiap.com.br>
2023-04-25 11:48:04 -0300

    Mudar Imagem

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -23,19 +23,76 @@
 //     }
 // )
 
-function soma(parametro){
-    num1= 1
-    num2= 2
-    console.log(num1 + num2)
-    console.log(`Isso é um patâmetro: ${parametro}`)
+// function soma(parametro){
+//     num1= 1
+//     num2= 2
+//     console.log(num1 + num2)
+//     console.log(`Isso é um patâmetro: ${parametro}`)
+// }
+
+// soma()
+
+
+
+// const mudaCor = ()=>{
+//     setTimeout(alert("penes"),5000)
+// }
+
+// mudaCor()
+
+// function mudaCor(){
+//     let r = "";
+//     let g = "";
+//     let b = "";
+   
+//     // Math.random
+//     // Math.ceil
+//     // Math.floor
+//     // Math.round
+//     r = Math.round(Math.random() * 255);
+//     g = Math.round(Math.random() * 255);
+//     b = Math.round(Math.random() * 255);
+//     console.log(r);
+//     console.log(g);
+//     console.log(b);
+
+//     const cabecalho = document.querySelector(".cabecalho")
+
+//     cabecalho.setAttribute("style", `background-color:rgb(${r},${g},${b})`)
+
+//     tmp = setTimeout(mudaCor, 1000)
+// }
+
+// mudaCor();
+
+function mudaImg1(){
+    const esquerda = document.querySelector(".l-e")
+    const direita = document.querySelector(".l-d")
+
+    esquerda.setAttribute("style", `background-image:url("./IMG/banner-lateral-2.png")`)
+    direita.setAttribute("style", `background-image:url("./IMG/banner-lateral-1.png")`)
+
+    tempo = setTimeout(mudaImg2, 1200)
 }
 
-soma()
+function mudaImg2(){
+    const esquerda = document.querySelector(".l-e")
+    const direita = document.querySelector(".l-d")
 
+    esquerda.setAttribute("style", `background-image:url("./IMG/banner-lateral-3.png")`)
+    direita.setAttribute("style", `background-image:url("./IMG/banner-lateral-2.png")`)
 
+    tempo = setTimeout(mudaImg3, 1200)
+}    
 
-const mudaCor = ()=>{
-    setTimeout(alert("penes"),5000)
-}
+function mudaImg3(){
+    const esquerda = document.querySelector(".l-e")
+    const direita = document.querySelector(".l-d")
 
-mudaCor()
+    esquerda.setAttribute("style", `background-image:url("./IMG/banner-lateral-1.png")`)
+    direita.setAttribute("style", `background-image:url("./IMG/banner-lateral-3.png")`)
+
+    tempo = setTimeout(mudaImg1, 1200)
+}    
+
+mudaImg1()
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 *** Exercitando o versionamento paizão
 
 1) Repositórios Locais e Remotos
+2) Spread
+3) Funções
+4)
